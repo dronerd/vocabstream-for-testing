7ffc833a2ab9cf37b59f20c5218a6995eeab014b
dronerd <novifuji555@gmail.com>
2025-10-09 11:00:26 +0900

    added page of progress transport

--- frontend/src/pages/Progress_transport.tsx
+++ frontend/src/pages/Progress_transport.tsx
@@ -1,9 +1,27 @@
-import React from "react";
+
+import React, { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
 export default function TestPage(){
+  const nav = useNavigate();
   return (
     <div
     style={{ paddingTop: "92px" }}>
-      <h2>学習経過を他のデバイスに移動</h2>
+      <button
+        onClick={() => nav("/ohers")}
+        style={{
+          marginBottom: 5,
+          padding: "8px 16px",
+          borderRadius: 8,
+          border: "none",
+          backgroundColor: "#555",
+          color: "#fff",
+          cursor: "pointer",
+        }}
+      >
+        その他の機能一覧に戻る
+      </button>
+      <h2>学習経過を他のデバイスに移動する</h2>
     </div>
   );
 }
